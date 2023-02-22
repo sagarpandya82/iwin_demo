0f686dbebe30fa8e75f175ab4f8ffe564ca0e46e
sagar pandya <sarg@outlook.com>
2023-02-22 10:37:22 +0000

    Add server-side validations

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,11 @@
 class Subscription < ApplicationRecord
   belongs_to :user
+
+  validates :card_expiry, comparison: { greater_than_or_equal_to: :today}
+  validates :cvc, comparison: { other_than: '999' }
+  private
+
+  def today
+    Date.today
+  end
 end
